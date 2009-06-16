e8b6086f6788be85d58ccb851cb6cbb53f3f1993
blancoj <>
2009-06-16 20:36:43 +0000

    new.

---
+++ bin/c/crms/monthlyStats.pl
@@ -0,0 +1,50 @@
+#!/l/local/bin/perl
+
+my $DLXSROOT;
+my $DLPS_DEV;
+
+BEGIN
+{
+    unshift( @INC, '/l1/dev/blancoj/cgi/c/crms' );
+}
+
+
+
+BEGIN 
+{ 
+    $DLXSROOT = $ENV{'DLXSROOT'}; 
+    $DLPS_DEV = $ENV{'DLPS_DEV'}; 
+}
+
+use strict;
+use CRMS;
+use Getopt::Std;
+use LWP::UserAgent;
+
+
+sub ReportMsg
+{
+  my ( $msg ) = @_;
+
+  my $newtime = scalar (localtime(time()));
+
+  $msg = qq{$newtime : $msg};
+  print "$msg","\n";
+
+}
+
+
+my $crms = CRMS->new(
+    logFile      =>   "$DLXSROOT/prep/c/crms/update_log.txt",
+    configFile   =>   "$DLXSROOT/bin/c/crms/crms.cfg",
+    verbose      =>   0,
+    root         =>   $DLXSROOT,
+    dev          =>   $DLPS_DEV,
+);
+
+$crms->CreateThisMonthsStats ();
+
+
+
+
+
