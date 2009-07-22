eda3d69785c7435ae6b8ef9a35ccd21abc48cb77
blancoj <>
2009-07-22 14:30:17 +0000

    added call to Jessica's script to update the rights db.

--- bin/c/crms/updateQueue.pl
+++ bin/c/crms/updateQueue.pl
@@ -9,6 +9,7 @@ BEGIN
 { 
     $DLXSROOT = $ENV{'DLXSROOT'}; 
     $DLPS_DEV = $ENV{'DLPS_DEV'}; 
+
     my $toinclude = qq{$DLXSROOT/cgi/c/crms};
     unshift( @INC, $toinclude );
 }
@@ -39,6 +40,7 @@ my $crms = CRMS->new(
     dev          =>   $DLPS_DEV,
 );
 
+
 my $msg = qq{Starting to Process the statuses. \n};
 &ReportMsg ( $msg );
 
@@ -52,8 +54,28 @@ my $msg = qq{Starting to create export for the rights db.  You should receive a 
 &ReportMsg ( $msg );
 ## get new items and load the queue table
 my $rc = $crms->ClearQueueAndExport();
+
+
+#only run this on dlps11
+#it will process all files in the rights_dir of the form *.rights and move them to the
+#/l1/prep/c/crms/archive
+my $host = `hostname`;
+if ( $host =~ m,dlsp11\..*, )
+{
+  my $msg = qq{Calling Jessica's script to populate the rights db.\n};
+  &ReportMsg ( $msg );
+
+  my $out = `/l/local/bin/perl /l1/bin/g/groove/populate_rights_data.pl --rights_dir=/l1/prep/c/crms --archive=/l1/prep/c/crms/archive/`;
+
+  my $msg = qq{DONE calling Jessica's script to populate the rights db.  This is the output:\n $out\n};
+  &ReportMsg ( $msg );
+}
+
 my $msg = qq{DONE Exporting. Starting to Load New volumes into candidates.\n};
 &ReportMsg ( $msg );
+
+
+
 
 ## get new items and load the queue table
 my $status = $crms->LoadNewItemsInCandidates ();
