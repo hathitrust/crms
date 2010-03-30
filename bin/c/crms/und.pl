b41a37b56f7dd0d79f69239c09ec0da53fe38cf5
moseshll <>
2010-03-30 22:37:54 +0000

    Initial checkin.

---
+++ bin/c/crms/und.pl
@@ -0,0 +1,93 @@
+#!/l/local/bin/perl
+
+# This script can be run from crontab; it 
+
+my $DLXSROOT;
+my $DLPS_DEV;
+BEGIN 
+{ 
+    $DLXSROOT = $ENV{'DLXSROOT'}; 
+    $DLPS_DEV = $ENV{'DLPS_DEV'}; 
+    unshift ( @INC, $ENV{'DLXSROOT'} . "/cgi/c/crms/" );
+}
+
+use strict;
+use CRMS;
+use Getopt::Std;
+
+my %opts;
+getopts('hnt:v', \%opts);
+
+my $help       = $opts{'h'};
+my $noop       = $opts{'n'};
+my $time       = $opts{'t'};
+my $verbose    = $opts{'v'};
+
+$time = 10800 unless $time;
+
+if ($help)
+{
+  die "USAGE: $0 [-h] [-n] [-t secs_to_run] [-v]\n\n";
+}
+
+my $file = $ARGV[0];
+
+my $crms = CRMS->new(
+    logFile      =>   "$DLXSROOT/prep/c/crms/und_hist.txt",
+    configFile   =>   'crms.cfg',
+    verbose      =>   $verbose,
+    root         =>   $DLXSROOT,
+    dev          =>   $DLPS_DEV
+);
+my $done = 0;
+my $of = 0;
+$SIG{ALRM} = sub { print "Signal received!\n" if $verbose; $done = 1;};
+my %und = ();
+alarm($time);
+my $sql = 'SELECT id FROM candidates WHERE checked=0';
+my $ref = $crms->get('dbh')->selectall_arrayref($sql);
+foreach my $row ( @{$ref} )
+{
+  last if $done;
+  my $id = $row->[0];
+  $of++;
+  print "Checking $id\n" if $verbose;
+  my $record = $crms->GetRecordMetadata($id);
+  my $lang = $crms->GetPubLanguage($id, $record);
+  if ('eng' ne $lang && '###' ne $lang && '|||' ne $lang && 'zxx' ne $lang && 'mul' ne $lang && 'sgn' ne $lang && 'und' ne $lang)
+  {
+    $und{$id} = 'language';
+    next;
+  }
+  if ($crms->IsThesis($id, $record))
+  {
+    $und{$id} = 'dissertation';
+    next;
+  }
+  if ($crms->IsTranslation($id, $record))
+  {
+    $und{$id} = 'translation';
+    next;
+  }
+  if ($crms->IsForeignPub($id, $record))
+  {
+    $und{$id} = 'foreign';
+    next;
+  }
+  $sql = "UPDATE candidates SET checked=1 WHERE id='$id'";
+  $crms->PrepareSubmitSql( $sql ) unless $noop;
+}
+
+my $n = scalar keys %und;
+foreach my $id (keys %und)
+{
+  my $src = $und{$id};
+  print "$id ($src) -> und table\n" if $verbose;
+  my $sql = "REPLACE INTO und (id,src) VALUES ('$id','$src')";
+  $crms->PrepareSubmitSql( $sql ) unless $noop;
+  $sql = "DELETE FROM candidates WHERE id='$id'";
+  $crms->PrepareSubmitSql( $sql ) unless $noop;
+}
+my $pct = 0.0;
+eval { $pct = 100.0 * $n / $of; };
+printf "Removed $n of $of (%0.2f%%)\n", $pct;
