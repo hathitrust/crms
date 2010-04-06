2d6dfa18a74cf42f29e05cfc5bbf8b0f97480ad9
moseshll <>
2010-04-06 20:58:30 +0000

    Final version.

--- bin/c/crms/und.pl
+++ bin/c/crms/und.pl
@@ -16,10 +16,11 @@ use CRMS;
 use Getopt::Std;
 
 my %opts;
-getopts('hnt:v', \%opts);
+getopts('hnpt:v', \%opts);
 
 my $help       = $opts{'h'};
 my $noop       = $opts{'n'};
+my $production = $opts{'p'};
 my $time       = $opts{'t'};
 my $verbose    = $opts{'v'};
 
@@ -27,7 +28,7 @@ $time = 10800 unless $time;
 
 if ($help)
 {
-  die "USAGE: $0 [-h] [-n] [-t secs_to_run] [-v]\n\n";
+  die "USAGE: $0 [-h] [-n] [-p] [-t secs_to_run] [-v]\n\n";
 }
 
 my $file = $ARGV[0];
@@ -37,7 +38,7 @@ my $crms = CRMS->new(
     configFile   =>   "$DLXSROOT/bin/c/crms/crms.cfg",
     verbose      =>   $verbose,
     root         =>   $DLXSROOT,
-    dev          =>   $DLPS_DEV
+    dev          =>   !$production
 );
 my $done = 0;
 my $of = 0;
@@ -55,46 +56,62 @@ foreach my $row ( @{$ref} )
   $of++;
   print "Checking $id\n" if $verbose;
   my $record = $crms->GetRecordMetadata($id);
-  my $lang = $crms->GetPubLanguage($id, $record);
-  if ('eng' ne $lang && '###' ne $lang && '|||' ne $lang && 'zxx' ne $lang && 'mul' ne $lang && 'sgn' ne $lang && 'und' ne $lang)
+  if ($record)
   {
-    $und{$id} = 'language';
-    $times{$id} = $time;
-    next;
+    my $lang = $crms->GetPubLanguage($id, $record);
+    if ($lang && 'eng' ne $lang && '###' ne $lang && '|||' ne $lang && 'zxx' ne $lang && 'mul' ne $lang && 'sgn' ne $lang && 'und' ne $lang)
+    {
+      $und{$id} = 'language';
+      $times{$id} = $time;
+      next;
+    }
+    elsif ($crms->IsThesis($id, $record))
+    {
+      $und{$id} = 'dissertation';
+      $times{$id} = $time;
+      next;
+    }
+    elsif ($crms->IsTranslation($id, $record))
+    {
+      $und{$id} = 'translation';
+      $times{$id} = $time;
+      next;
+    }
+    elsif ($crms->IsForeignPub($id, $record))
+    {
+      $und{$id} = 'foreign';
+      $times{$id} = $time;
+      next;
+    }
+    $sql = "UPDATE candidates SET checked=1 WHERE id='$id'";
+    $crms->PrepareSubmitSql($sql) unless $noop;
   }
-  if ($crms->IsThesis($id, $record))
-  {
-    $und{$id} = 'dissertation';
-    $times{$id} = $time;
-    next;
-  }
-  if ($crms->IsTranslation($id, $record))
-  {
-    $und{$id} = 'translation';
-    $times{$id} = $time;
-    next;
-  }
-  if ($crms->IsForeignPub($id, $record))
-  {
-    $und{$id} = 'foreign';
-    $times{$id} = $time;
-    next;
-  }
-  $sql = "UPDATE candidates SET checked=1 WHERE id='$id'";
-  $crms->PrepareSubmitSql( $sql ) unless $noop;
 }
 
 my $n = scalar keys %und;
+my %counts = ('language' => 0, 'dissertation' => 0, 'translation' => 0, 'foreign' => 0);
 foreach my $id (keys %und)
 {
   my $src = $und{$id};
   my $time = $times{$id};
-  print "$id ($src) -> und\n";
+  $counts{$src}++;
+  print "$id ($src)\n";
   my $sql = "REPLACE INTO und (id,src,time) VALUES ('$id','$src','$time')";
-  $crms->PrepareSubmitSql( $sql ) unless $noop;
+  $crms->PrepareSubmitSql($sql) unless $noop;
   $sql = "DELETE FROM candidates WHERE id='$id'";
-  $crms->PrepareSubmitSql( $sql ) unless $noop;
+  $crms->PrepareSubmitSql($sql) unless $noop;
 }
 my $pct = 0.0;
 eval { $pct = 100.0 * $n / $of; };
 printf "Removed $n of $of (%0.2f%%)\n", $pct;
+foreach my $src (sort keys %counts)
+{
+  printf "$src: %d\n", $counts{$src};
+}
+
+my $r = $crms->GetErrors();
+foreach my $w (@{$r})
+{
+  print "Warning: $w\n";
+}
+
