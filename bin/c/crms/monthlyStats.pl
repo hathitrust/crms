cd1ae0caca5ad8d9f45180cac10b744c64ced424
moseshll <>
2010-04-28 18:15:28 +0000

    Fix previous update breaking usability by cron in production.

--- bin/c/crms/monthlyStats.pl
+++ bin/c/crms/monthlyStats.pl
@@ -19,14 +19,15 @@ use LWP::UserAgent;
 
 my %opts;
 getopts('p', \%opts);
-my $production = $opts{'p'};
+my $dev = $DLPS_DEV;
+$dev = 0 if $opts{'p'};
 
 my $crms = CRMS->new(
     logFile      =>   "$DLXSROOT/prep/c/crms/log_monthlyStats.txt",
     configFile   =>   "$DLXSROOT/bin/c/crms/crms.cfg",
     verbose      =>   0,
     root         =>   $DLXSROOT,
-    dev          =>   !$production
+    dev          =>   $dev
 );
 
 $crms->UpdateStats();
