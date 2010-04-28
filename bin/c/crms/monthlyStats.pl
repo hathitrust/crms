829768390558e17e943a048a3beaf5d074024f4a
moseshll <>
2010-04-28 18:12:23 +0000

    Add -p flag for running against production.

--- bin/c/crms/monthlyStats.pl
+++ bin/c/crms/monthlyStats.pl
@@ -17,13 +17,16 @@ use CRMS;
 use Getopt::Std;
 use LWP::UserAgent;
 
+my %opts;
+getopts('p', \%opts);
+my $production = $opts{'p'};
 
 my $crms = CRMS->new(
     logFile      =>   "$DLXSROOT/prep/c/crms/log_monthlyStats.txt",
     configFile   =>   "$DLXSROOT/bin/c/crms/crms.cfg",
     verbose      =>   0,
     root         =>   $DLXSROOT,
-    dev          =>   $DLPS_DEV,
+    dev          =>   !$production
 );
 
 $crms->UpdateStats();
