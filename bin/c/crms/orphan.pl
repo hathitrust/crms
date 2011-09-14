5bde583563c0b37a2786c7915f61c1cbbf6cc03a
moseshll <>
2011-09-14 18:13:02 +0000

    Modifications to report format requested by Bobby and Julia.

--- bin/c/crms/orphan.pl
+++ bin/c/crms/orphan.pl
@@ -18,34 +18,36 @@ use Spreadsheet::WriteExcel;
 use Encode;
 
 my $usage = <<END;
-USAGE: $0 [-hiptv] -n N [-s VOL_ID [-s VOL_ID2...]]
+USAGE: $0 [-ahipv] [-n N] [-r TYPE] [-s VOL_ID [-s VOL_ID2...]]
           [-m MAIL_ADDR [-m MAIL_ADDR2...]]
 
 Creates a report of recent exports for the orphan works project.
 
+-a         Ignore the -n flag and export all.
 -h         Print this help message.
 -i         Insert the volumes into the orphan table.
 -m ADDR    Mail the report to ADDR. May be repeated for multiple addresses.
 -n N       Export no more than N volumes. (Default is 3000.)
 -p         Run in production.
 -r TYPE    Print a report of TYPE where TYPE={html,none,tsv,excel}.
-           In the case of Excel it will be created in place and
-           attached to any outgoing mail. Default is html.
+           In the case of excel it will be created in place and
+           attached to any outgoing mail. Default is excel.
 -s VOL_ID  Report only for HT volume VOL_ID. May be repeated for multiple volumes.
--v         Be verbose.
+-v         Be verbose. May be repeated.
 END
-
+my $all;
 my $help;
 my $insert;
 my @mails;
 my $n;
 my $production;
-my $report = 'html';
+my $report = 'excel';
 my @singles;
 my $verbose;
 
 Getopt::Long::Configure ('bundling');
-die 'Terminating' unless GetOptions('h|?' => \$help,
+die 'Terminating' unless GetOptions('a' => \$all,
+           'h|?' => \$help,
            'i' => \$insert,
            'm:s@' => \@mails,
            'n:s' => \$n,
@@ -75,7 +77,7 @@ $n = 3000 unless $n and $n > 0;
 my $sql = "SELECT id,gid FROM exportdata WHERE attr='ic' AND reason='ren'";
 if (@singles && scalar @singles)
 {
-  $sql = sprintf("SELECT id,gid FROM exportdata WHERE attr='ic' AND reason='ren' AND id in ('%s')", join "','", @singles);
+  #$sql = sprintf("SELECT id,gid FROM exportdata WHERE attr='ic' AND id in ('%s')", join "','", @singles);
   $sql = sprintf("SELECT id,gid FROM exportdata WHERE id in ('%s')", join "','", @singles);
 }
 $sql .= ' AND src!="inherited" AND id NOT IN (SELECT id FROM orphan)';
@@ -87,8 +89,12 @@ my $txt = '';
 my $title = "CRMS Orphan Works Report $now";
 my ($workbook,$worksheet);
 my $excelpath = sprintf('/l1/prep/c/crms/OrphanCand_%s.xls', $now);
-my @cols= ('ID','Renewal #','Renewal Date','Title','Author Last Name','Author First Name','Author Dates',
-           'Publisher 1','Publisher 2','Publisher 3','Publisher 4');
+my @cols= ('#', 'ID','Renewal #','Renewal Date','Title','Author Last Name','Author First Name','Author Dates',
+           'Publisher 1 Location','Publisher 1 Name','Publisher 1 Year',
+           'Publisher 2 Location','Publisher 2 Name','Publisher 2 Year',
+           'Publisher 3 Location','Publisher 3 Name','Publisher 3 Year',
+           'Publisher 4 Location','Publisher 4 Name','Publisher 4 Year');
+my $pub1NameIdx = 8; # For sorting (# is prepended after the sort)
 if ($report eq 'html')
 {
   $txt .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' . "\n";
@@ -109,7 +115,8 @@ elsif ($report eq 'excel')
 }
 my $found = 0;
 my %seen = ();
-printf "%d volumes found, $n sought\n", scalar @{$ref} if $verbose;
+printf "%d volumes found, %s sought\n", scalar @{$ref}, ($all)?'all':$n if $verbose;
+my @rows = ();
 foreach my $row (@{$ref})
 {
   my $id = $row->[0];
@@ -123,10 +130,10 @@ foreach my $row (@{$ref})
   my $ref2 = $dbh->selectall_arrayref($sql);
   next unless $ref2 && scalar @{$ref2};
   my ($attr,$reason,$src,$usr,$time,$note) = @{$crms->RightsQuery($id,1)->[0]};
-  next unless $attr eq 'ic' and $reason eq 'ren';
+  next unless $attr eq 'ic';
   $seen{$id} = 1;
   $found++;
-  print "Found: $found of $n\n" if $verbose;
+  printf "Found: $found of %s\n", ($all)?'all':$n if $verbose;
   my ($renNum,$renDate) = @{$ref2->[0]};
   my $sysid = $crms->BarcodeToId($id);
   my $record = $crms->GetMetadata($sysid);
@@ -135,37 +142,39 @@ foreach my $row (@{$ref})
   my $title = $crms->GetRecordTitle($id, $record);
   my $dates = GetRecordAuthorDates($id, $record);
   my @fields = ($id, $renNum, $renDate, $title, $authlast, $authrest, $dates);
-  my @pubs = ();
+  my @pubs = (['','',''],['','',''],['','',''],['','','']);
+  my $pubn = 0;
   my $nodes = $record->findnodes("//*[local-name()='datafield' and \@tag='260']/*[local-name()='subfield']");
   my $code = '';
   my $lastcode = undef;
+  my %h = ('a'=>0,'b'=>1,'c'=>2);
   foreach my $node ($nodes->get_nodelist())
   {
     printf "Doing a subfield: $node (%s)\n", $node->localname if $verbose > 2;
     my $code = lc $node->getAttribute('code');
     my $field = $node->textContent();
+    $field =~ s/[,\.]+\s*$// if $code eq 'c';
     print "Field: '$field' for '$code'\n" if $verbose > 1;
-    if ($code eq 'a' || ($lastcode && $code le $lastcode))
-    {
-      if ($code eq 'a' && $lastcode && $lastcode eq 'a')
-      {
-        $pubs[-1] .= ', ' . $field;
-      }
-      else
-      {
-        last if scalar @pubs == 4;
-        push @pubs, $field;
-      }
-    }
-    else
-    {
-      push @pubs, '' unless scalar @pubs;
-      $pubs[-1] .= ' '. $field;
-    }
+    # If we are seeing a code same as or less than the one we last saw, advance.
+    $pubn++ if $lastcode and $code le $lastcode;
+    last if $pubn == 5;
+    $pubs[$pubn]->[$h{$code}] = $field;
+    #printf "pubs[$pubn]->%s = '$field' from '$code'\n", $h{$code};
     $lastcode = $code;
+    #print "pubn $pubn, code $code, lastcode $lastcode\n";
   }
-  push @fields, (($_+1<=scalar @pubs)?$pubs[$_]:'') for (0 .. 3);
+  push @fields, @{$pubs[$_]} for (0 .. 3);
   printf "Have %d pubs, %d fields\n", scalar @pubs, scalar @fields if $verbose > 1;
+  push @rows, join '____', @fields;
+  $crms->PrepareSubmitSql("INSERT INTO orphan (id) VALUES ('$id')") if $insert;
+  last if !$all and $found >= $n;
+}
+$found = 0;
+foreach my $row (SortByPub(\@rows))
+{
+  my @fields = split m/____/, $row;
+  $found++;
+  unshift @fields, $found;
   if ($report eq 'html')
   {
     $txt .= sprintf("<tr><td>%s</td></tr>\n", join '</td><td>', map {s/&/&amp;/g;$_;} @fields);
@@ -180,8 +189,6 @@ foreach my $row (@{$ref})
   {
     $worksheet->write_string($found, $_, $fields[$_]) for (0 .. scalar @fields);
   }
-  $crms->PrepareSubmitSql("INSERT INTO orphan (id) VALUES ('$id')") if $insert;
-  last if $found >= $n;
 }
 if ($report eq 'html')
 {
@@ -208,7 +215,7 @@ if (@mails)
   $sender->Body();
   if ($report eq 'excel')
   {
-    $txt = "Attached please find $n ic/ren volumes to be considered for the Orphan Works Project.\n"; 
+    $txt = "Attached please find $found ic/ren volumes to be considered for the Orphan Works Project.\n"; 
   }
   my $bytes = encode('utf8', $txt);
   $sender->SendEnc($bytes);
@@ -238,6 +245,29 @@ sub GetRecordAuthorDates
 
   my $data = $crms->GetMarcDatafield($id,'100','d',$record);
   $data = $crms->GetMarcDatafield($id,'700','d',$record) unless $data;
+  my $len = length $data;
+  if ($len && $len % 3 == 0)
+  {
+    my $s = $len / 3;
+    my $f1 = substr $data, 0, $s;
+    my $f2 = substr $data, $s, $s;
+    my $f3 = substr $data, 2*$s, $s;
+    #print "'$f1' + '$f2' + '$f3' from '$data' ($id)\n";
+    $data = $f1 if $f1 eq $f2 and $f2 eq $f3;
+  }
   $data =~ s/[\.,:;]\s*$//;
   return $data;
+}
+
+sub SortByPub
+{
+  my $ref = shift;
+
+  return sort {
+    my @aar = split m/____/, $a;
+    my @bar = split m/____/, $b;
+    my $aa = lc $aar[$pub1NameIdx];
+    my $ba = lc $bar[$pub1NameIdx];
+    $aa cmp $ba;
+  } @{$ref};
 }
