b6962ef8c07efd041e0575d076f6cc77385ba4b9
moseshll <>
2013-01-09 19:47:03 +0000

    Move IsProbableGovDoc() from CRMS.pm to Candidates_crms.pm as it is US-only.

--- cgi/c/crms/Candidates_crms.pm
+++ cgi/c/crms/Candidates_crms.pm
@@ -60,13 +60,68 @@ sub ShouldVolumeGoInUndTable
   $record = $self->GetMetadata($id) unless $record;
   return 'no meta' unless $record;
   my $lang = $self->GetRecordPubLanguage($id, $record);
-  if ($self->IsProbableGovDoc($id, $record)) { $src = 'gov'; }
+  if (IsProbableGovDoc($self, $id, $record)) { $src = 'gov'; }
   elsif ('eng' ne $lang) { $src = 'language'; }
   elsif ($self->IsThesis($id, $record)) { $src = 'dissertation'; }
   elsif ($self->IsTranslation($id, $record)) { $src = 'translation'; }
   # FIXME: Move IsReallyForeignPub over here?
   elsif ($self->IsReallyForeignPub($id, $record)) { $src = 'foreign'; }
   return $src;
+}
+
+# An item is a probable gov doc if one of the following is true. All are case insensitive.
+# Author begins with "United States" and 260 is blank
+# Author begins with "United States" and 260a begins with "([)Washington"
+# Author begins with "United States" and 260b begins with "U.S. G.P.O." or "U.S. Govt. Print. Off."
+# Author begins with "Library of Congress" and 260a begins with "Washington"
+# Title begins with "Code of Federal Regulations" and 260a begins with "Washington"
+# Author is blank and 260(a) begins with "([)Washington" and 260(b) begins with "U.S."
+# Author is blank and 260(a) begins with "([)Washington" and 260(b) begins with "G.P.O."
+# Author is blank and 260(b) includes "National Aeronautics and Space"
+# Author begins with "Federal Reserve Bank"
+# Author includes "Bureau of Mines"
+sub IsProbableGovDoc
+{
+  my $self   = shift;
+  my $id     = shift;
+  my $record = shift;
+
+  $record = $self->GetMetadata($id) unless $record;
+  if (!$record) { $self->SetError("no record in IsProbableGovDoc: $id"); return 0; }
+  my $author = $self->GetRecordAuthor($id, $record);
+  my $title = $self->GetRecordTitle($id, $record);
+  my $xpath  = q{//*[local-name()='datafield' and @tag='260']/*[local-name()='subfield' and @code='a']};
+  my $field260a = $record->findvalue($xpath);
+  $xpath  = q{//*[local-name()='datafield' and @tag='260']/*[local-name()='subfield' and @code='b']};
+  my $field260b = $record->findvalue($xpath);
+  $field260a =~ s/^\s*(.*?)\s*$/$1/;
+  $field260b =~ s/^\s*(.*?)\s*$/$1/;
+  # If there is an alphabetic character in 008:28 other than 'f',
+  # we accept it and say it is NOT probable
+  $xpath  = q{//*[local-name()='controlfield' and @tag='008']};
+  my $leader = lc $record->findvalue($xpath);
+  my $code = substr($leader, 28, 1);
+  return 0 if ($code ne 'f' && $code =~ m/[a-z]/);
+  if ($author =~ m/^united\s+states/i)
+  {
+    return 1 unless $field260a or $field260b;
+    return 1 if $field260a =~ m/^\[?washington/i;
+    return 1 if $field260b and $field260b =~ m/^u\.s\.\s+g\.p\.o\./i;
+    return 1 if $field260b and $field260b =~ m/^u\.s\.\s+govt\.\s+print\.\s+off\./i;
+  }
+  return 1 if $author =~ m/^library\s+of\s+congress/i and $field260a =~ m/^washington/i;
+  return 1 if $title =~ m/^code\s+of\s+federal\s+regulations/i and $field260a =~ m/^washington/i;
+  if (!$author)
+  {
+    return 1 if $field260a =~ m/^\[?washington/i and $field260b =~ m/^(u\.s\.|g\.p\.o\.)/i;
+    return 1 if $field260b and $field260b =~ m/national\s+aeronautics\s+and\s+space/i;
+  }
+  else
+  {
+    return 1 if $author =~ m/^federal\s+reserve\s+bank/i;
+    return 1 if $author =~ m/bureau\s+of\s+mines/i;
+  }
+  return 0;
 }
 
 1;
