9c9c161565081afd71cbe540f54be5556ad09d81
moseshll <>
2013-07-08 21:02:13 +0000

    Modernization and fixup.

--- bin/c/crms/loadSCRD.pl
+++ bin/c/crms/loadSCRD.pl
@@ -11,11 +11,11 @@ BEGIN
 
 use strict;
 use CRMS;
-use Getopt::Std;
+use Getopt::Long qw(:config no_ignore_case bundling);
 
 
 my $usage = <<END;
-USAGE: ./loadSCRD.pl [-hnpt] [-x SYS] FILE [FILE...]
+USAGE: $0 [-hnptv] [-x SYS] FILE [FILE...]
 
 Loads a downloaded copy of the Stanford Cpoyright Renewal Database
 into the CRMS database "stanford" table.
@@ -24,16 +24,20 @@ into the CRMS database "stanford" table.
 -n       Don't actually do anything, just simulate.
 -p       Run in production.
 -t       Run in training (overrides -p).
+-v       Emit debugging information.
 -x SYS   Set SYS as the system to execute.
 END
 
-my %opts;
-getopts('hnptx', \%opts);
-my $help       = $opts{'h'};
-my $noop       = $opts{'n'};
-my $production = $opts{'p'};
-my $training   = $opts{'t'};
-my $sys        = $opts{'x'};
+my ($help, $noop, $production, $training, $verbose, $sys);
+Getopt::Long::Configure('bundling');
+die 'Terminating' unless GetOptions(
+           'h|?'  => \$help,
+           'n'    => \$noop,
+           'p'    => \$production,
+           't'    => \$training,
+           'v'    => \$verbose,
+           'x:s'  => \$sys);
+
 $DLPS_DEV = undef if $production;
 $DLPS_DEV = 'crmstest' if $training;
 die $usage if $help or !scalar @ARGV;
@@ -55,47 +59,48 @@ foreach my $filename (@ARGV)
 sub ProcessFile
 {
   my $file = shift;
-  open (my $fh, $file) || die "failed to open $file: $@ \n";
-  my ($id,$dreg);
-  my $n = 0;
-  foreach my $line (<$fh>)
+  open my $in, $file or die "failed to open $file: $! \n";
+  read $in, my $buff, -s $file; # one of many ways to slurp file.
+  close $in;
+  $buff =~ s/\s+$//;
+  my @chunks = split m/\n\n|----+/, $buff;
+  printf "$file: %s chunks\n", scalar @chunks;
+  foreach my $chunk (@chunks)
   {
-    $n++;
-    chomp $line;
-    $line =~ s/\r+//gs;
-    if ($line =~ m/^---/ && $id && $dreg)
+    chomp $chunk;
+    my $id = undef;
+    my $dreg = undef;
+    foreach my $line (split m/[\r\n]+/, $chunk)
     {
-      AddRecord($id, $dreg, $file);
-      $id = undef;
-      $dreg = undef;
-      next;
-    }
-    my ($tag,$val) = split(/\:/, $line, 2);
-    $tag =~ s/\s+//gs;
-    $val =~ s/\s+//gs;
-    #print "Tag <$tag> value <$val>\n";
-    if ($tag eq "ID")
-    {
-      $id = $val;
-      $id =~ s/(RE?\d+).*/$1/;
-      if ($id !~ m/^RE?\d+$/)
+      $line =~ s/\r+//gs;
+      my ($tag,$val) = split(/\:/, $line, 2);
+      $tag =~ s/\s+//gs;
+      $val =~ s/\s+//gs;
+      print "Tag <$tag> value <$val>\n" if $verbose > 1;
+      if ($tag eq "ID")
       {
-        printf "Bogus ID format: '$val' -- ignoring. ($file) %s\n", ($dreg)? $dreg:'';
-        $id = undef;
+        $id = $val;
+        $id =~ s/(RE?\d+).*/$1/;
+        if ($id !~ m/^RE?\d+$/)
+        {
+          printf "Bogus ID format: '$val' -- ignoring. ($file) %s\n", ($dreg)? $dreg:'';
+          $id = undef;
+        }
+      }
+      elsif ($tag eq "DREG")
+      {
+        $dreg = $val;
+        $dreg =~ s/(\d+[A-Za-z][A-Za-z][A-Za-z]\d\d).*/$1/;
+        if ($dreg !~ m/^\d+[A-Za-z][A-Za-z][A-Za-z]\d\d$/)
+        {
+          printf "Bogus DREG format: '$val' -- ignoring. ($file) %s\n";
+          $dreg = undef;
+        }
       }
     }
-    if ($tag eq "DREG")
-    {
-      $dreg = $val;
-      $dreg =~ s/(\d+[A-Za-z][A-Za-z][A-Za-z]\d\d).*/$1/;
-      if ($dreg !~ m/^\d+[A-Za-z][A-Za-z][A-Za-z]\d\d$/)
-      {
-        printf "Bogus DREG format: '$val' -- ignoring. ($file) %s\n";
-        $dreg = undef;
-      }
-    }
+    print "ID: $id, DREG: $dreg\n" if $verbose > 1;
+    AddRecord($id, $dreg, $file) if defined $id and defined $dreg;
   }
-  close $fh;
 }
 
 sub AddRecord
@@ -103,12 +108,8 @@ sub AddRecord
   my $id       = shift;
   my $dreg     = shift;
   my $filename = shift;
-  
+
   my $sql = "REPLACE INTO stanford (ID, DREG) VALUES (?,?)";
-  if ($noop)
-  {
-    #print "$filename: $sql\n";
-    return;
-  }
-  $crms->PrepareSubmitSql($sql, $id, $dreg);
+  print "$filename: $sql ($id, $dreg)\n" if $verbose;
+  $crms->PrepareSubmitSql($sql, $id, $dreg) unless $noop;
 }
