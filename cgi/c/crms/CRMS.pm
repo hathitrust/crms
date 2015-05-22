3cce681718fb44eb5b274550d1b51abccd019021
moseshll <>
2015-05-22 18:30:15 +0000

    Migrate HexDump() to Utilities.pm.

--- cgi/c/crms/CRMS.pm
+++ cgi/c/crms/CRMS.pm
@@ -6786,38 +6786,6 @@ sub CountCorrectReviews
   return ($correct,$incorrect,$neutral);
 }
 
-# Utility (I used it for debugging UTF-8 problems)
-sub HexDump
-{
-  my $self = shift;
-  $_       = shift;
-
-  my $offset = 0;
-  my(@array,$format);
-  my $dump = '';
-  foreach my $data (unpack("a16"x(length($_[0])/16)."a*",$_[0]))
-  {
-    my($len)=length($data);
-    if ($len == 16)
-    {
-      @array = unpack('N4', $data);
-      $format="0x%08x (%05d)   %08x %08x %08x %08x   %s\n";
-    }
-    else
-    {
-      @array = unpack('C*', $data);
-      $_ = sprintf "%2.2x", $_ for @array;
-      push(@array, '  ') while $len++ < 16;
-      $format="0x%08x (%05d)" .
-           "   %s%s%s%s %s%s%s%s %s%s%s%s %s%s%s%s   %s\n";
-    }
-    $data =~ tr/\0-\37\177-\377/./;
-    $dump .= sprintf $format,$offset,$offset,@array,$data;
-    $offset += 16;
-  }
-  return $dump;
-}
-
 # Gets only those reviewers that are not experts
 sub GetType1Reviewers
 {
