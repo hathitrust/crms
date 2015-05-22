b7b388739f295a767a9429d725d79df44aa0e22b
moseshll <>
2015-05-22 18:29:53 +0000

    Initial checkin.

---
+++ cgi/c/crms/Utilities.pm
@@ -0,0 +1,36 @@
+package Utilities;
+
+use strict;
+use warnings;
+
+sub HexDump
+{
+  my $self = shift;
+  $_       = shift;
+
+  my $offset = 0;
+  my(@array,$format);
+  my $dump = '';
+  foreach my $data (unpack("a16"x(length($_[0])/16)."a*",$_[0]))
+  {
+    my($len)=length($data);
+    if ($len == 16)
+    {
+      @array = unpack('N4', $data);
+      $format="0x%08x (%05d)   %08x %08x %08x %08x   %s\n";
+    }
+    else
+    {
+      @array = unpack('C*', $data);
+      $_ = sprintf "%2.2x", $_ for @array;
+      push(@array, '  ') while $len++ < 16;
+      $format="0x%08x (%05d)" .
+           "   %s%s%s%s %s%s%s%s %s%s%s%s %s%s%s%s   %s\n";
+    }
+    $data =~ tr/\0-\37\177-\377/./;
+    $dump .= sprintf $format,$offset,$offset,@array,$data;
+    $offset += 16;
+  }
+  return $dump;
+}
+
