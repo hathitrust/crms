1be45ffb8c5755c50bff6e5f6f5c37d2b353d520
moseshll <>
2015-07-29 21:29:49 +0000

    Moved to js/ directory.

--- web/c/crms/common.js
+++
@@ -1,7 +0,0 @@
-function setCookie(name,value,expiredays)
-{
-  var exdate = new Date();
-  exdate.setDate(exdate.getDate()+expiredays);
-  document.cookie = name + "=" + escape(value)+
-    ((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
-}
