422c8a9164d93b2cd14810ba4355fe5710bf4d4f
moseshll <>
2017-10-19 17:40:59 +0000

    Remove unused routines.

--- web/c/crms/js/review.js
+++ web/c/crms/js/review.js
@@ -219,38 +219,6 @@ function PredictDate(id)
   req.send(null);
 }
 
-// FIXME: the following four functions appear to be unused
-function ShowPopup(hoveritem)
-{
-  hp = document.getElementById("hoverpopup");
-
-  // Set position of hover-over popup
-  hp.style.top = hoveritem.offsetTop + 18;
-  hp.style.left = hoveritem.offsetLeft + 20;
-
-  // Set popup to visible
-  hp.style.visibility = "Visible";
-}
-
-function HidePopup()
-{
-  hp = document.getElementById("hoverpopup");
-  hp.style.visibility = "none";
-}
-
-function change()
-{
-  hp = document.getElementById("test");
-  hp.style.color = "blue";
-}
-
-function toggle()
-{
-  hp = document.getElementById(1);
-  var color = hp.checked? "red" : "red";
-  hp.style.color = color;
-}
-
 function ToggleADD()
 {
   var lab = document.getElementById("renewalFieldLabel");
