15f5a770d183f1250baefaa4109649efa3ceb467
Moses Hall <moseshll@umich.edu>
2018-10-12 14:09:37 -0400

    Remove unused training.pl options.

--- bin/training.pl
+++ bin/training.pl
@@ -1,12 +1,8 @@
 #!/usr/bin/perl
 
-my ($root);
 BEGIN 
 { 
-  $root = $ENV{'SDRROOT'};
-  $root = $ENV{'DLXSROOT'} unless $root and -d $root;
-  unshift(@INC, $root. '/crms/cgi');
-  unshift(@INC, $root. '/cgi/c/crms');
+  unshift(@INC, $ENV{'SDRROOT'}. '/crms/cgi');
 }
 
 use strict;
@@ -17,18 +13,13 @@ use Term::ANSIColor qw(:constants);
 $Term::ANSIColor::AUTORESET = 1;
 
 my $usage = <<END;
-USAGE: $0 [-7Ddhnqv] [-e N] [-x SYS] [-p PROJ [-p PROJ2...]] COUNT
+USAGE: $0 [-7Ddhnqv] [-x SYS] [-p PROJ [-p PROJ2...]] COUNT
 
 Populates the training database with examples (correct, single reviews)
 from production so that the queue size is increased to COUNT.
 
--7       Disallow status 7 determinations.
 -D       Delete all existing reviews and volumes in queue.
 -d       Run in dev (training otherwise).
--e N     Specify an ease factor from 1 to 3 inclusive:
-         1. Use only Status 4 determinations, no Inserts.
-         2. Use only Status 4 and 5 determinations, no Inserts.
-         3. (default) Use all Status 4 and 5 determinations.
 -h       Print this help message.
 -n       Do not submit SQL.
 -p PROJ  Only include reviews on the specified project PROJ. Defaults to 'Core'.
@@ -37,10 +28,8 @@ from production so that the queue size is increased to COUNT.
 -x SYS   Set SYS as the system to execute.
 END
 
-my $no7;
 my $del;
 my $dev;
-my $ease;
 my $help;
 my $instance;
 my $noop;
@@ -51,10 +40,8 @@ my $sys;
 
 Getopt::Long::Configure ('bundling');
 die 'Terminating' unless GetOptions(
-           '7'    => \$no7,
            'D'    => \$del,
            'd'    => \$dev,
-           'e:3'  => \$ease,
            'h'    => \$help,
            'n'    => \$noop,
            'p:s@' => \@projs,
@@ -66,9 +53,6 @@ die "$usage\n\n" if $help;
 die "You need a volume count.\n" unless 1 == scalar @ARGV;
 my $count = $ARGV[0];
 die "Count format should be numeric\n" if $count !~ m/^\d+$/;
-die "Ease should be numeric\n" if defined $ease && $ease !~ m/^\d+$/;
-die "Ease should be between 1 and 3 inclusive\n" if defined $ease && ($ease < 1 || $ease > 3);
-$ease = 3 unless defined $ease;
 
 my $crmsp = CRMS->new(
     sys          =>   $sys,
@@ -92,51 +76,50 @@ if ($verbose)
   print "Training instance: $dbinfo\n";
   
 }
-
+my %seen;
+my %seenAuthorTitles;
+my %seenSysids;
 if ($del)
 {
   print "Deleting reviews and emptying queue...\n" if $verbose;
   $crmst->PrepareSubmitSql('DELETE from queue');
   $crmst->PrepareSubmitSql('DELETE from reviews');
 }
-
-### Get a list of ids from the training DB already seen,
-### and populate the 'seen' hash with them.
-my %seen;
-my %seenAuthorTitles;
-my %seenSysids;
-my $sql = 'SELECT q.id,b.sysid,b.author,b.title FROM queue q INNER JOIN bibdata b ON q.id=b.id';
-my $ref = $crmst->SelectAll($sql);
-foreach my $row (@{$ref})
+else
 {
-  my $id = $row->[0];
-  my $sysid = $row->[1];
-  my $a = $row->[2] || '';
-  my $t = $row->[3] || '';
-  my $at = $a . $t;
-  $at =~ s/[^A-Za-z0-9]//g;
-  print "Updating seen filters for existing $id ($sysid) $at\n" if $verbose;
-  $seen{$id} = 1;
-  $seenSysids{$sysid} = 1;
-  $seenAuthorTitles{$at} = 1 if length $at;
+  ### Get a list of ids from the training DB already seen,
+  ### and populate the 'seen' hash with them.
+  my $sql = 'SELECT q.id,b.sysid,b.author,b.title FROM queue q'.
+            ' INNER JOIN bibdata b ON q.id=b.id';
+  my $ref = $crmst->SelectAll($sql);
+  foreach my $row (@{$ref})
+  {
+    my $id = $row->[0];
+    my $sysid = $row->[1];
+    my $a = $row->[2] || '';
+    my $t = $row->[3] || '';
+    my $at = $a . $t;
+    $at =~ s/[^A-Za-z0-9]//g;
+    print "Updating seen filters for existing $id ($sysid) $at\n" if $verbose;
+    $seen{$id} = 1;
+    $seenSysids{$sysid} = 1;
+    $seenAuthorTitles{$at} = 1 if length $at;
+  }
 }
 my $n = 0;
 my $usql = '(SELECT id FROM users WHERE reviewer=1 AND expert+admin=0)';
-my $ssql = 'e.status=4';
-$ssql .= ' OR e.status=5' unless $ease == 1;
-$ssql .= ' OR e.status=7' unless $no7;
 @projs = ('Core') unless scalar @projs;
 my $projsql = sprintf 'p.name IN ("%s")', join '","', @projs;
-$sql = 'SELECT r.id,r.user,r.time,r.gid,e.status,p.name FROM historicalreviews r'.
-       ' INNER JOIN exportdata e ON r.gid=e.gid'.
-       ' INNER JOIN projects p ON e.project=p.id'.
-       ' WHERE r.user IN '. $usql.
-       ' AND '. $projsql.
-       ' AND e.ticket IS NULL'.
-       ' AND r.validated=1 AND e.reason!="crms" AND ('. $ssql. ')'.
-       ' ORDER BY r.time DESC';
+my $sql = 'SELECT r.id,r.user,r.time,e.status,p.name FROM historicalreviews r'.
+          ' INNER JOIN exportdata e ON r.gid=e.gid'.
+          ' INNER JOIN projects p ON e.project=p.id'.
+          ' WHERE r.user IN '. $usql.
+          ' AND '. $projsql.
+          ' AND e.ticket IS NULL'.
+          ' AND r.validated=1 AND (e.status=4 OR e.status=5 OR e.status=7)'.
+          ' ORDER BY r.time DESC';
 
-$ref = $crmsp->SelectAll($sql);
+my $ref = $crmsp->SelectAll($sql);
 printf "$sql: %s results\n", (defined $ref)? scalar @$ref:'no' if $verbose;
 my $s4 = 0;
 my $s5 = 0;
@@ -179,21 +162,13 @@ foreach my $row (@{$ref})
   $seenAuthorTitles{$at} = 1 if length $at;
   my $user   = $row->[1];
   my $time   = $row->[2];
-  my $gid    = $row->[3];
-  my $status = $row->[4];
-  my $proj   = $row->[5];
+  my $status = $row->[3];
+  my $proj   = $row->[4];
   # Disallow swissed volumes.
   $sql = 'SELECT MAX(swiss) FROM historicalreviews WHERE id=?';
   if (0 < $crmsp->SimpleSqlGet($sql, $id))
   {
     print RED "Skipping swissed $id\n" if $verbose;
-    next;
-  }
-  # Disallow inserts unless ease is 3.
-  $sql = 'SELECT COUNT(*) FROM historicalreviews WHERE gid=? AND category="Insert(s)"';
-  if (0 < $crmsp->SimpleSqlGet($sql, $gid) && $ease < 3)
-  {
-    print RED "Skipping Inserts on $id for ease $ease\n" if $verbose;
     next;
   }
   $sql = 'SELECT COALESCE(id,1) FROM projects WHERE name=?';
