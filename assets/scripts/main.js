5cdf3affc5c9e3f134e9d390a3dec1f8c00e803b
Moses Hall <moseshll@umich.edu>
2025-07-02 11:03:00 -0400

    Add missing assets/scripts/main.js

---
+++ assets/scripts/main.js
@@ -0,0 +1,12 @@
+//import 'tippy/dist/css/bootstrap.min.css'
+
+import 'tippy.js/dist/tippy.css';
+import 'highcharts/css/highcharts.css';
+
+import tippy from 'tippy.js';
+window.tippy = tippy;
+
+//import Highcharts from 'highcharts/esm/highcharts.js';
+
+import * as crms from './crms.js';
+window.crms = crms;
