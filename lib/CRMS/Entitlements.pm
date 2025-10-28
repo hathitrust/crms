02b3d51830a729e37ff63ccdc8c265a15525d926
Brian Moses Hall <moseshll@umich.edu>
2025-10-28 12:52:03 -0400

    Add `CRMS::Entitlements` intended to eventually replace the attributes/reasons/rights routines in CRMS.pm with a more structured class that is not just a wrapper around SQL queries:   - `TranslateAttr`   - `TranslateReason`   - `TranslateRights`   - `AllCRMSRights`   - `Rights`   - `AllAssignableRights`   - `GetAttrReasonFromCode`   - `TranslateAttrReasonFromCode`   - `GetCodeFromAttrReason`   - `GetAttrReasonCode`
    This class is fully tested. For now it will only be used in the SBCR module and its tests.

---
+++ lib/CRMS/Entitlements.pm
@@ -0,0 +1,126 @@
+package CRMS::Entitlements;
+
+# NOTE THIS IS A TEMPORARY NAME
+# CRMS::Rights conflicts with a method defined in the CRMS module
+# Once this module is proven, it can replace some/all of the rights/attributes/reasons
+# methods in CRMS.pm
+
+# Manages an in-memory copy of the crms.rights table
+# and through it the attributes and reasons it ties together.
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+# This is a singleton. Rights, attributes, and reasons are static and can be cached.
+# Derivatives based on project_rights, if any, should not be cached because they can change.
+my $ONE_TRUE_ENTITLEMENTS;
+
+sub new {
+  my ($class, %args) = @_;
+  my $self = bless {}, $class;
+  # TODO: once we have a standalone DB module this can go away.
+  my $crms = $args{crms};
+  die "CRMS::Entitlements module needs CRMS instance." unless defined $crms;
+  $self->{crms} = $crms;
+  # Eager load lookup tables
+  $self->_load_tables;
+  if (!defined $ONE_TRUE_ENTITLEMENTS) {
+    $ONE_TRUE_ENTITLEMENTS = $self;
+  }
+  return $ONE_TRUE_ENTITLEMENTS;
+}
+
+sub rights_by_id {
+  my $self = shift;
+  my $id   = shift;
+
+  return $self->{rights}->{$id};
+}
+
+sub rights_by_attribute_reason {
+  my $self      = shift;
+  my $attribute = shift;
+  my $reason    = shift;
+
+  # Translate attribute and reason into ids if not numeric
+  if ($attribute !~ m/^\d+$/) {
+    $attribute = $self->attribute_by_name($attribute)->{id};
+  }
+  if ($reason !~ m/^\d+$/) {
+    $reason = $self->reason_by_name($reason)->{id};
+  }
+  foreach my $id (keys %{$self->{rights}}) {
+    my $rights = $self->{rights}->{$id};
+    if ($rights->{attr} == $attribute && $rights->{reason} == $reason) {
+      return $rights;
+    }
+  }
+}
+
+# Returns a hashref with the fields id, type, dscr, name just as they appear in the
+# `attributes` table
+sub attribute_by_id {
+  my $self = shift;
+  my $id   = shift;
+
+  return $self->{attributes_by_id}->{$id};
+}
+
+# Returns a hashref with the fields id, type, dscr, name just as they appear in the
+# `attributes` table
+sub attribute_by_name {
+  my $self = shift;
+  my $name = shift;
+
+  return $self->{attributes_by_name}->{$name};
+}
+
+# Returns a hashref with the fields id, dscr, name just as they appear in the
+# `reasons` table
+sub reason_by_id {
+  my $self = shift;
+  my $id   = shift;
+
+  return $self->{reasons_by_id}->{$id};
+}
+
+# Returns a hashref with the fields id, dscr, name just as they appear in the
+# `reasons` table
+sub reason_by_name {
+  my $self = shift;
+  my $name = shift;
+
+  return $self->{reasons_by_name}->{$name};
+}
+
+# Set up slightly duplicative lookup tables for fast attribute/reason access by id or by name.
+# Also set up rights lookup by id.
+sub _load_tables {
+  my $self = shift;
+
+  # crms.attributes
+  my $sql = 'SELECT * FROM attributes ORDER BY id';
+  $self->{attributes_by_id} = $self->{crms}->GetDb->selectall_hashref($sql, 'id');
+  $self->{attributes_by_name} = $self->{crms}->GetDb->selectall_hashref($sql, 'name');
+  # crms.reasons
+  $sql = 'SELECT * FROM reasons ORDER BY id';
+  $self->{reasons_by_id} = $self->{crms}->GetDb->selectall_hashref($sql, 'id');
+  $self->{reasons_by_name} = $self->{crms}->GetDb->selectall_hashref($sql, 'name');
+  # crms.rights
+  $self->{rights} = {};
+  $sql = 'SELECT * FROM rights ORDER BY id';
+  $self->{rights} = $self->{crms}->GetDb->selectall_hashref($sql, 'id');
+  # Decorare each entry with attribute and reason names
+  foreach my $id (keys %{$self->{rights}}) {
+    my $rights = $self->{rights}->{$id};
+    my $attr_name = $self->attribute_by_id($rights->{attr})->{name};
+    my $reason_name = $self->reason_by_id($rights->{reason})->{name};
+    $rights->{attribute_name} = $attr_name;
+    $rights->{reason_name} = $reason_name;
+    $rights->{name} = "$attr_name/$reason_name";
+  }
+}
+
+1;
