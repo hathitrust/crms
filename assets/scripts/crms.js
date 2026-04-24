0b390e92b631b6e8a89177364fa2b88c2f278eb9
Moses Hall <moseshll@umich.edu>
2026-04-24 12:26:03 -0400

    - Update one remaining reference to `GetCheckedValue` which is now `getCheckedValue` - Tidy JS formatting

--- assets/scripts/crms.js
+++ assets/scripts/crms.js
@@ -10,9 +10,7 @@ export function toggleVisibility(id) {
   if (me == null)
   {
     //alert("Can't get " + id);
-  }
-  else
-  {
+  } else {
     if (me.style.display!="none") {me.style.display="none";}
     else {me.style.display="";}
   }
@@ -23,17 +21,11 @@ export function toggleVisibility(id) {
 // document.getElementById(id).value = value
 export function selectOption(id, value) {
   var me = document.getElementById(id);
-  if (me == null)
-  {
+  if (me == null) {
     //alert("Can't get " + id);
-  }
-  else
-  {
-    var i;
-    for (i = 0; i < me.length; i++)
-    {
-      if (me.options[i].value == value)
-      {
+  } else {
+    for (var i = 0; i < me.length; i++) {
+      if (me.options[i].value == value) {
         me.selectedIndex = i;
         break;
       }
@@ -52,26 +44,20 @@ export function syncCheckboxToForm(box, formid) {
 
 export function sfHover() {
   var sfEls = document.getElementById("menu").getElementsByTagName("li");
-  for (var i=0; i<sfEls.length; i++)
-  {
-    sfEls[i].onmouseover=function()
-    {
+  for (var i=0; i<sfEls.length; i++) {
+    sfEls[i].onmouseover=function() {
       this.className+=" sfhover";
     }
-    sfEls[i].onmouseout=function()
-    {
+    sfEls[i].onmouseout=function() {
       this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
     }
   }
   var sfAs = document.getElementById("menu").getElementsByTagName("a");
-  for (var i=0; i<sfAs.length; i++)
-  {
-    sfAs[i].onmouseover=function()
-    {
+  for (var i=0; i<sfAs.length; i++) {
+    sfAs[i].onmouseover=function() {
       this.className+=" sfhover";
     }
-    sfAs[i].onmouseout=function()
-    {
+    sfAs[i].onmouseout=function() {
       this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
     }
   }
@@ -82,8 +68,7 @@ export function changeFrame1(doSize) {
   var url = sel.options[sel.selectedIndex].value;
   var tf = document.getElementById("tFrame");
   if (tf.src != url) { tf.src = url; }
-  if (doSize)
-  {
+  if (doSize) {
     tf.style.display = "block";
     document.getElementById("bFrame").style.display = "none";
   }
@@ -94,40 +79,32 @@ export function changeFrame2(doSize) {
   var url = sel.options[sel.selectedIndex].value;
   var bf = document.getElementById("bFrame");
   if (bf.src != url) { bf.src = url; }
-  if (doSize)
-  {
+  if (doSize) {
     bf.style.display = "block";
     document.getElementById("tFrame").style.display = "none";
   }
 }
 
-export function flipFrame()
-{
+export function flipFrame() {
   var tf = document.getElementById("tFrame");
   var bf = document.getElementById("bFrame");
-  if (tf.style.display == "none")
-  {
+  if (tf.style.display == "none") {
     tf.style.display = "block";
     bf.style.display = "none";
-  }
-  else
-  {
+  } else {
     tf.style.display = "none";
     bf.style.display = "block";
   }
 }
 
-export function popRenewalDate()
-{
+export function popRenewalDate() {
   var renNum = document.getElementById('renewalField');
   var renDate = document.getElementById('getDate');
   var id  = renNum.value;
   renDate.value = "Searching...";
   var req = new XMLHttpRequest();
-  req.onreadystatechange = function()
-  {
-    if (req.readyState == 4)
-    {
+  req.onreadystatechange = function() {
+    if (req.readyState == 4) {
       var icren = document.getElementById('ICREN');
       var pdusren = document.getElementById('PDUSREN');
       // TODO: this is a really clunky way of making the mapping from rights name ->
@@ -135,16 +112,13 @@ export function popRenewalDate()
       // Ideally, review.tt should query Entitlements.pm and construct JSON with name -> id map
       // as a global variable.
       var icrenButton, pdusrenButton;
-      if (icren)
-      {
+      if (icren) {
         icrenButton = document.getElementById("r" + icren.title);
       }
-      if (pdusren)
-      {
+      if (pdusren) {
         pdusrenButton = document.getElementById("r" + pdusren.title);
       }
-      if (req.status == 200)
-      {
+      if (req.status == 200) {
         var rights = document.getElementsByName("rights");
         var sel = getCheckedValue(rights);
         var und = document.getElementById('UNDNFI').title;
@@ -154,17 +128,14 @@ export function popRenewalDate()
         var cutoff = new String(new Date().getFullYear() - 69).slice(-2);
         var pdus = req.responseText.endsWith(cutoff);
         if (und && sel == und) { return; }
-        if (pdus)
-        {
+        if (pdus) {
           if (pdusrenButton) { pdusrenButton.checked = "checked"; }
         }
-        else
-        {
+        else {
           if (icrenButton) { icrenButton.checked = "checked"; }
         }
       }
-      else
-      {
+      else {
         renDate.value = "";
         if (icrenButton) { icrenButton.checked = ""; }
         if (pdusrenButton) { pdusrenButton.checked = ""; }
@@ -206,7 +177,7 @@ function displayRightPrediction(data) {
 // Internal function
 function deselectCurrentRights() {
   var rights = document.getElementsByName("rights");
-  var sel = GetCheckedValue(rights);
+  var sel = getCheckedValue(rights);
   if (sel) {
     document.getElementById("r" + sel).checked = false;
   }
@@ -226,20 +197,17 @@ export function togglePredictionLoader(display) {
   }
 }
 
-// return the value of the radio button that is checked
-// return an empty string if none are checked, or
-// there are no radio buttons
+// Return the value of the radio button that is checked
+// Return `null` if none are checked, or there are no radio buttons
 export function getCheckedValue(radioObj) {
   if (!radioObj) { return null; }
   var radioLength = radioObj.length;
-  if (radioLength == undefined)
-  {
+  if (radioLength == undefined) {
     if (radioObj.checked) { return radioObj.value; }
     else { return null; }
   }
   var i;
-  for (i = 0; i < radioLength; i++)
-  {
+  for (i = 0; i < radioLength; i++) {
     if (radioObj[i].checked) { return radioObj[i].value; }
   }
   return null;
@@ -249,13 +217,10 @@ export function getCheckedValue(radioObj) {
 // so not exporting.
 function Debug(msg, append) {
   var el = document.getElementById('debugArea');
-  if (el)
-  {
-    if (append)
-    {
+  if (el) {
+    if (append) {
       msg = el.innerHTML + "<br/>\n" + msg;
     }
     el.innerHTML = msg;
   }
 }
-
