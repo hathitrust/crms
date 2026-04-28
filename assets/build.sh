845960f130d0546da7b378cb9bc691f80ae7d81c
Brian "Moses" Hall <moseshll@umich.edu>
2026-04-28 12:20:53 -0400

    Remove stale comments

--- assets/build.sh
+++ assets/build.sh
@@ -1,2 +1,1 @@
 npx esbuild "assets/scripts/**/main.js" --entry-names=[dir]/[name] --outbase=assets/scripts --bundle --minify --outdir=public/scripts $*
-#npx esbuild "assets/scripts/**/review.js" --entry-names=[dir]/[name] --outbase=assets/scripts --bundle --minify --outdir=public/scripts $*
--- assets/build_css.sh
+++ assets/build_css.sh
@@ -1,4 +1,2 @@
-# FIXME: probably don't need the $* parameter?
-# Can I take all of these and squish them all into a single main.css file?
 sass $* assets/styles:public/styles --style=compressed --load-path=node_modules
 sass $* node_modules/tippy.js/dist/tippy.css public/styles/tippy.css
